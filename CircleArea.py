e19b45367fc5ae49b3ee69ae1e193f281805fa45
Umairjavaidbutt <52698829+Umairjavaidbutt@users.noreply.github.com>
2020-02-12 17:40:50 +0500

    Update CircleArea.py

--- CircleArea.py
+++ CircleArea.py
@@ -1,14 +1,7 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 pi = 3.14
 def calcArea(r):
     return pi * r**2
     
-print(calcArea(8.7))
-print(calcArea(9.2))
-print(calcArea(16.03))
+print(round(calcArea(8.7),2))
+print(round(calcArea(9.2),2))
+print(round(calcArea(16.03),2))
