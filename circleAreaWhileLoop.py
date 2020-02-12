07f05b101833fd8bcdfc2156f7131e38a370ec85
Umairjavaidbutt <52698829+Umairjavaidbutt@users.noreply.github.com>
2020-02-12 18:01:45 +0500

    Update circleAreaWhileLoop.py

--- circleAreaWhileLoop.py
+++ circleAreaWhileLoop.py
@@ -1,13 +1,15 @@
-
 def calArea(r):
     pi = 3.14
     return pi * r **2
     
 while True:
-    r = input("Enter circle radius, pree 'q' to quit: ")
+    r = input("Enter circle radius, enter 'q' to quit: ")
     if r == 'q':
       break
     else:
-      r = float(r)
-      print(f"Area of circle is {calArea(r)}")
-
+        try:
+            r = float(r)
+        except:
+            print("Enter int or float only.")
+            continue
+        print(f"Area of circle is {calArea(r)}")
